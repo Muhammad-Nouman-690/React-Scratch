c81ebb305ab8ace6b07d1403ce6d65c4cb9a558c
Muhammad-Nouman-690 <nomi690@hotmail.com>
2021-01-06 12:48:16 +0500

    Hand Made React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <h1>
+      Hey, Nouman!
+      <br />
+      Welcome to Hand Made React World..!
+    </h1>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
