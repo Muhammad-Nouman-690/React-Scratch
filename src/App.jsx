48fef964c150969c65a5e3c92409a803635e0245
Muhammad-Nouman-690 <nomi690@hotmail.com>
2021-01-06 13:04:22 +0500

    set css clases

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './App.css'
 
 const App = () => {
   return (
-    <h1>
+    <h1 className='App'>
       Hey, Nouman!
       <br />
+      <br />
       Welcome to Hand Made React World..!
+      <br />
+      <br />
+      This project i'll create without Create-react-app.
     </h1>
   )
 };
